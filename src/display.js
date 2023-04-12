9cad65d949394d0b7f3131d956ef9d4e54dbb9d3
Caylin Vu <caylinvu@gmail.com>
2023-04-12 18:32:49 -0500

    Add highlight to selected tab

--- src/display.js
+++ src/display.js
@@ -1,6 +1,17 @@
-function displayTaskBtn() {
-    const mainContent = document.querySelector('.main-content');
+import { allTasks, today, thisWeek, important, tabs } from './index';
 
+const mainContent = document.querySelector('.main-content');
+const contentHeader = document.querySelector('.content-heading');
+
+function highlightSelected(selectedTab) {
+    tabs.forEach((tab) => {
+        tab.classList.remove('selected');
+    });
+
+    selectedTab.classList.toggle('selected');
+}
+
+function displayTaskBtn() {
     const taskBtn = document.createElement('button');
     taskBtn.classList.add('task-btn');
 
@@ -15,10 +26,35 @@ function displayTaskBtn() {
     mainContent.appendChild(taskBtn);
 }
 
-function displayAllTasks() {
-    const contentHeader = document.querySelector('.content-heading');
-    contentHeader.textContent = 'All Tasks';
-    displayTaskBtn();
+function removeTaskBtn() {
+    if (mainContent.lastChild.className === 'task-btn') {
+        mainContent.removeChild(mainContent.lastChild);
+    }
 }
 
-export { displayAllTasks }
+function displayAllTasks() {
+    removeTaskBtn();
+    displayTaskBtn();
+    highlightSelected(allTasks);
+    contentHeader.textContent = 'All Tasks';
+}
+
+function displayToday() {
+    removeTaskBtn();
+    highlightSelected(today);
+    contentHeader.textContent = 'Today';
+}
+
+function displayThisWeek() {
+    removeTaskBtn();
+    highlightSelected(thisWeek);
+    contentHeader.textContent = 'This Week';
+}
+
+function displayImportant() {
+    removeTaskBtn();
+    highlightSelected(important);
+    contentHeader.textContent = 'Important';
+}
+
+export { displayAllTasks, displayToday, displayThisWeek, displayImportant }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import { displayAllTasks } from './display';
+import { displayAllTasks, displayToday, displayThisWeek, displayImportant } from './display';
 import { addTask } from './createTask';
 
+const allTasks = document.getElementById('all-tasks');
+const today = document.getElementById('today');
+const thisWeek = document.getElementById('this-week');
+const important = document.getElementById('important');
+const tabs = [allTasks, today, thisWeek, important];
+
 displayAllTasks();
+
+allTasks.addEventListener('click', displayAllTasks);
+today.addEventListener('click', displayToday);
+thisWeek.addEventListener('click', displayThisWeek);
+important.addEventListener('click', displayImportant);
+
+export { allTasks, today, thisWeek, important, tabs }
